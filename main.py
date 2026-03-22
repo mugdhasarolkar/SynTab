83cc1801e06b03ed14153e64902d590072c72426
Mugdha <mugdha.sarolkar@gmail.com>
2026-03-22 10:46:33 +0530

    Corrected code to train model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from preprocessing.preprocess import load_data
 from preprocessing.preprocess import preprocess
+from models.vae_model import build_vae
+from models.train import train_vae
 data_path=r"E:\MyMLproject\syntab\data\raw\loan_data.csv"
 df=load_data(data_path)
 df_processed,scaler,encoder,num_imputer,cat_imputer=preprocess(df)
@@ -7,3 +9,5 @@ X=df_processed.to_numpy()
 input_dim=X.shape[1]
 print(type(X))
 print(X.shape)
+build_vae(input_dim)
+train_vae(X)
--- models/vae_model.py
+++ models/vae_model.py
@@ -1,34 +1,79 @@
 import tensorflow as tf
-from keras import layers,Model
-import numpy as np
+from keras import ops
+from keras import layers, Model
+
 def build_vae(input_dim):
-    h1=min(128,max(8,input_dim//2))
-    h2=min(64,max(4,input_dim//4))
-    latent_dim=max(2,input_dim//8)
-    #Encoder
-    encoder_input=tf.keras.Input(shape=(input_dim,),name="encoder_input")
-    x=layers.Dense(h1,activation="relu")(encoder_input)
-    x=layers.Dense(h2,activation="relu")(x)
-    mu=layers.Dense(latent_dim,name="latent_mu")(x)
-    log_var=layers.Dense(latent_dim,name="latent_log_var")(x)
+    h1 = min(128, max(8, input_dim // 2))
+    h2 = min(64, max(4, input_dim // 4))
+    latent_dim = max(2, input_dim // 8)
+
+    # Encoder
+    encoder_input = tf.keras.Input(shape=(input_dim,), name="encoder_input")
+
+    x = layers.Dense(h1, activation="relu")(encoder_input)
+    x = layers.Dense(h2, activation="relu")(x)
+
+    mu = layers.Dense(latent_dim, name="latent_mu")(x)
+    log_var = layers.Dense(latent_dim, name="latent_log_var")(x)
 
     def sampling(args):
-        mu,log_var=args
-        epsilon=tf.random.normal(shape=tf.shape(mu))
-        return mu+tf.exp(0.5*log_var)*epsilon
-    z=layers.Lambda(sampling,name="z")([mu,log_var])
-    encoder=Model(encoder_input,[mu,log_var],name="encoder")
+        mu, log_var = args
+        epsilon = tf.random.normal(shape=tf.shape(mu))
+        return mu + tf.exp(0.5 * log_var) * epsilon
 
-    #Decoder
-    latent_input=layers.Input(shape=(latent_dim,),name="z_sampling")
-    x=layers.Dense(h2,activation="relu")(latent_input)
-    x=layers.Dense(h1,activation="relu")(x)
-    output=layers.Dense(input_dim,activation="sigmoid")(x)
-    decoder=Model(latent_input,output,name="decoder")
-    vae_output=decoder(encoder(encoder_input)[2])
-    vae=Model(encoder_input,vae_output,name="vae")
-    #add losses
-    kl_loss=-0.5*tf.reduce_mean(tf.reduce_sum(1+log_var-tf.square(mu)-tf.exp(log_var),axis=1))
-    reconstruction_loss=tf.reduce_mean(tf.reduce_sum(tf.square(encoder_input-vae_output),axis=1))
-    vae.add_loss(reconstruction_loss+kl_loss)
-    return vae,encoder,decoder
+    z = layers.Lambda(sampling, name="z")([mu, log_var])
+
+    encoder = Model(encoder_input, [mu, log_var, z], name="encoder")
+
+    # Decoder
+    latent_input = layers.Input(shape=(latent_dim,), name="z_sampling")
+
+    x = layers.Dense(h2, activation="relu")(latent_input)
+    x = layers.Dense(h1, activation="relu")(x)
+    output = layers.Dense(input_dim, activation="sigmoid")(x)
+
+    decoder = Model(latent_input, output, name="decoder")
+
+    # VAE Model (Custom Training)
+    class VAE(Model):
+        def __init__(self, encoder, decoder):
+            super().__init__()
+            self.encoder = encoder
+            self.decoder = decoder
+
+        def call(self, inputs):
+            mu, log_var, z = self.encoder(inputs)
+            return self.decoder(z)
+
+        def train_step(self, data):
+            with tf.GradientTape() as tape:
+                mu, log_var, z = self.encoder(data)
+                reconstruction = self.decoder(z)
+
+                # Reconstruction Loss
+                reconstruction_loss = ops.mean(
+                    ops.sum(ops.square(data - reconstruction), axis=1)
+                )
+
+                # KL Divergence Loss
+                kl_loss = -0.5 * ops.mean(
+                    ops.sum(
+                        1 + log_var - ops.square(mu) - ops.exp(log_var),
+                        axis=1
+                    )
+                )
+
+                total_loss = reconstruction_loss + kl_loss
+
+            grads = tape.gradient(total_loss, self.trainable_weights)
+            self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+
+            return {
+                "loss": total_loss,
+                "reconstruction_loss": reconstruction_loss,
+                "kl_loss": kl_loss,
+            }
+
+    vae = VAE(encoder, decoder)
+
+    return vae, encoder, decoder
